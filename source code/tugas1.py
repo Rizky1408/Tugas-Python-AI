b3964077ebd233ae8b6f276340cd81269ad208e4
Rizky1408 <agungdwisatria23@gmail.com>
2021-10-28 12:51:52 +0800

    uploud

---
+++ source code/tugas1.py
@@ -0,0 +1,8 @@
+print("masukan panjang")
+panjang = int(input())
+print("masukan lebar")
+lebar = int(input())
+luas = panjang * lebar
+keliling = 2 * (panjang + lebar)
+print("Luas Persegi panjang adalah :" + str(luas))
+print("Keliling persegi panjang :" + str(keliling))
---
+++ source code/tugas5.py
@@ -0,0 +1,3 @@
+c = float(input())
+f = c * (float(9) / 5) + 32
+print("Maka suhu dalam Fahrenhait : " + str(f))
